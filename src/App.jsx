7617b809764575ebded04841eae7b7d90aba9372
crucie <amaymishra100@gmail.com>
2025-04-13 20:23:51 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+
+  return (
+    <>
+      <div>
+        BLOG IT
+      </div>
+    </>
+  )
+}
+
+export default App
