455d7f434472de67251f4e3b925faceef20043ba
crucie <amaymishra100@gmail.com>
2025-04-20 23:49:07 +0530

    worked on Store and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService, { AuthService } from './appwrite/auth'
+import {login, logout} from './store/authSlice'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const  [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData){
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='w-screen'>
       <div>
-        BLOG IT
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/> 
       </div>
-    </>
-  )
+    </div>
+  ) : (null)
 }
 
 export default App
+ 
