d28d2f4fa6c015af0f1876672ff39c14fe7ad9e6
kshimate <amaymishra100@gmail.com>
2025-06-29 23:32:37 +0530

    updated some file

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -14,14 +14,16 @@ function Select({
         {label && 
         <label htmlFor={id} className=''></label>}
         <select
-        className={` px-3 py-2 rounded-lg bg-white text-black outline-none hover:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-        ref={ref}
-        id={id}
-        {...props}>
-            {options?.map((option) =>
-            <option key={option} value={option}>
-                {option}
-            </option>)}
+            className={` px-3 py-2 rounded-lg bg-white text-black outline-none hover:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            ref={ref}
+            id={id}
+            {...props}>
+
+                {options?.map((option) =>
+                <option key={option} value={option}>
+                    {option}
+                </option>)}
+
         </select>
     </div>
   )
